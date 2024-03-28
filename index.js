e23b7e4d3e4990413d9cd632807442ea312eea90
Valderum <45272079+Valderum@users.noreply.github.com>
2024-03-28 16:34:00 +0000

    Create index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+const { exec } = require('child_process');
+
+// Function to call a Python script
+function runPythonScript() {
+    // Adjust the path according to your Python script's location
+    const pythonScriptPath = 'lib/model.py;
+
+    exec(`python ${pythonScriptPath}`, (error, stdout, stderr) => {
+        if (error) {
+            console.error(`exec error: ${error}`);
+            return;
+        }
+        if (stderr) {
+            console.error(`stderr: ${stderr}`);
+            return;
+        }
+        console.log(`Python script output: ${stdout}`);
+    });
+}
+
+// Call the function to run the Python script
+runPythonScript();
